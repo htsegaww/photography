53674e85a6f997284b44c92355322c38825f3e74
Henok <hennok100@gmai.com>
2024-04-15 12:43:25 -0400

    added theme from shadcn

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThemeToggle from "./ThemeToggle";
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggle />
+    </header>
+  );
+};
+
+export default Header;
