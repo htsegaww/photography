a8203e936cc9a4a092a91cea59e89fd18e7dd839
Henok <hennok100@gmai.com>
2024-04-17 20:01:15 -0400

    added Features component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Features />
+      <Services />
     </main>
   );
 }
---
+++ components/Services.jsx
@@ -0,0 +1,12 @@
+import { Smile, Scan, Bird } from "ludide-react";
+
+const serviceData = [
+  {
+    icon: <Smile />,
+  },
+];
+const Services = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
